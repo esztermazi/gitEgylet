105cacd07e3719bd5088e67d11fff979e55ef283
MiklosHorvath <horvathmikloswork@gmail.com>
2019-09-10 11:08:41 +0200

    Implements choose color

--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,48 @@
-let playBtns = document.getElementsByClassName('play-button');
-
 function guessRow(event) {
     console.log("play pressed");
 }
 
 
-for (playBtn of playBtns) {
-    playBtn.addEventListener('click', guessRow);
+function chooseColour(event) {
+    let colours = ["red", "blue", "yellow", "orange", "green", "violet"];
+    if (event.target.classList.contains("empty")){
+        event.target.classList.remove("empty");
+        event.target.classList.add("red");
+    } else {
+        for(let index = 0; index<colours.length;index++){
+            if(event.target.classList.contains("violet")){
+                event.target.classList.remove("violet");
+                event.target.classList.add("red");
+            } else {
+                if(event.target.classList.contains(colours[index])){
+                    event.target.classList.remove(colours[index]);
+                    event.target.classList.add(colours[index+1]);
+                    break;
+                }
+            }
+        }
+    }
 }
+
+
+function addEventLinstenerToPlayBtn(){
+    let playBtns = document.getElementsByClassName('play-button');
+    for (playBtn of playBtns) {
+        playBtn.addEventListener('click', guessRow);
+    }
+}
+
+
+function addEventListenerToGuestCells() {
+    let guessCells = document.getElementsByClassName('guess-cell');
+    for (guessCell of guessCells) {
+        guessCell.addEventListener('click', chooseColour);
+    }
+}
+
+
+function main() {
+    addEventLinstenerToPlayBtn();
+    addEventListenerToGuestCells();
+}
+main();
